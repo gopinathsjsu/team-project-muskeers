493e3cb6b13c45fb39051e31dc107e2a106869ef
Alihussain Ladiwala <alihussainladiwala@gmail.com>
2021-10-30 13:23:40 -0700

    added routing and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+
+
+const LoginContainer = () => (
+  // eslint-disable-next-line react/jsx-filename-extension
+  <Container >
+    <Route component={Login} />
+  </Container>
+);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={LoginContainer} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
