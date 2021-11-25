9babf5063363bc846952889df2836b315e58347c
karan221997 <karan.jariwala221997@gmail.com>
2021-11-25 13:11:19 -0800

    added start time in api

--- server/routes/FlightDetails.js
+++ server/routes/FlightDetails.js
@@ -9,7 +9,7 @@ router.get("/flightDetails", (req, res) => {
     const source = req.body.source;
     const destination = req.body.destination;
 
-    const flightDetailsQuery = "select flight_table.flight_id, flight_table.source_city, flight_table.destination_city, flight_details.end_time, flight_details.availability, flight_details.price, flight_details.start_date, flight_details.status from flight_table INNER JOIN flight_details ON flight_table.flight_id = flight_details.flight_id WHERE flight_table.source_city = ? and flight_table.destination_city = ? and flight_details.start_date =? and flight_details.availability > 0;";
+    const flightDetailsQuery = "select flight_table.flight_id, flight_table.source_city, flight_table.destination_city, flight_details.end_time, flight_details.availability, flight_details.price, flight_details.start_date, flight_details.status, flight_details.start_time from flight_table INNER JOIN flight_details ON flight_table.flight_id = flight_details.flight_id WHERE flight_table.source_city = ? and flight_table.destination_city = ? and flight_details.start_date =? and flight_details.availability > 0;";
     pool.query(flightDetailsQuery, [source,destination,startDate], (err, result) => {
         console.log(result);
         console.log(err);
@@ -26,7 +26,8 @@ router.get("/flightDetails", (req, res) => {
             end_time: result[0].end_time,
             availability: result[0].availability,
             price: result[0].price,
-            status: result[0].status
+            status: result[0].status,
+            start_time: result[0].start_time
 
         });
     });
